cdfd7d9d67fbf5e5a27b8e69d6fb41bc35b102f4
Jkapella <jvkapella@gmail.com>
2019-06-17 18:49:21 +0100

    added index and includes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const ObjectId = mongo.ObjectId;
+
+//db setup
+const url = 'mongodb://localhost:27017';
+const dbName = 'listeningList';
+const client = new MongoClient(url);
+
+//core app setup
+const app = express();
+const port = 9001;
+
+var jsonParser = bodyParser.json();
