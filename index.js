c91fc416617be7ea21b83d379a42b9dc5694ff58
Jkapella <jvkapella@gmail.com>
2019-06-20 18:45:29 +0100

    added allowance for CORS policy

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const port = 9001;
 
 var jsonParser = bodyParser.json();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
+    next();
+})
+
 
 app.route('/ratedAlbums')
     .get((req, res) => {
